eb23da8409d12208ab484be9d64aa7c7f9fbed02
punjwaniakbar <66209936+punjwaniakbar@users.noreply.github.com>
2021-11-14 21:35:34 +0500

    fix: cors error with ajax fetch requests in click.js

--- Project_Starter_Files-Building_a_Metrics_Dashboard/reference-app/frontend/static/js/click.js
+++ Project_Starter_Files-Building_a_Metrics_Dashboard/reference-app/frontend/static/js/click.js
@@ -4,7 +4,13 @@ $(document).ready(function () {
     // http://backend.observability.svc.cluster.local
     $("#firstbutton").click(function () {
         $.ajax({
-            url: "http://localhost:3003", success: function (result) {
+            url: "http://localhost:3003", 
+            dataType: 'jsonp',
+            crossDomain: true,
+            headers: {
+                'Access-Control-Allow-Origin': '*'
+                },
+            success: function (result) {
                 $("#firstbutton").toggleClass("btn-primary:focus");
                 }
         });
@@ -13,7 +19,13 @@ $(document).ready(function () {
     //http://trial.observability.svc.cluster.local
     $("#secondbutton").click(function () {
         $.ajax({
-            url: "http://localhost:3004", success: function (result) {
+            url: "http://localhost:3004", 
+            dataType: 'jsonp',
+            crossDomain: true,
+            headers: {
+                'Access-Control-Allow-Origin': '*'
+                },
+            success: function (result) {
                 $("#secondbutton").toggleClass("btn-primary:focus");
             }
         });
