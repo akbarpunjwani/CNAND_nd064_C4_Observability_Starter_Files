1df2feb6ead757b694023def71f83ef0a9200f21
punjwaniakbar <66209936+punjwaniakbar@users.noreply.github.com>
2021-11-14 20:20:09 +0500

    fix: url for button clicks

--- Project_Starter_Files-Building_a_Metrics_Dashboard/reference-app/frontend/static/js/click.js
+++ Project_Starter_Files-Building_a_Metrics_Dashboard/reference-app/frontend/static/js/click.js
@@ -1,16 +1,19 @@
 $(document).ready(function () {
 
     // all custom jQuery will go here
+    // http://backend.observability.svc.cluster.local
     $("#firstbutton").click(function () {
         $.ajax({
-            url: "http://backend.default.svc.cluster.local", success: function (result) {
+            url: "http://localhost:3003", success: function (result) {
                 $("#firstbutton").toggleClass("btn-primary:focus");
                 }
         });
     });
+
+    //http://trial.observability.svc.cluster.local
     $("#secondbutton").click(function () {
         $.ajax({
-            url: "http://trial.default.svc.cluster.local", success: function (result) {
+            url: "http://localhost:3004", success: function (result) {
                 $("#secondbutton").toggleClass("btn-primary:focus");
             }
         });
